fbc2bf8ec8ae6e9e3404e190cdc00032bc6dfb2f
Chris Kansas <ckansas@smcm.edu>
2018-03-30 22:22:27 +0000

    Renamed campgrounds.ejs to index.ejs to follow RESTful convetions. Added a show page for further info, updated data schema to have a description; updated data with descriptions.

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ mongoose.connect('mongodb://localhost/camp_app');
 
 var campgroundSchema = new mongoose.Schema({
    name: String,
-   image: String
+   image: String,
+   description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -25,7 +26,7 @@ app.get('/campgrounds', function(req, res){
       if(err){
          console.log(err);
       } else {
-         res.render('campgrounds', {campgrounds: allCampgrounds}); 
+         res.render('index', {campgrounds: allCampgrounds}); 
       }
    });
 });
@@ -33,7 +34,8 @@ app.get('/campgrounds', function(req, res){
 app.post('/campgrounds', function(req, res){
    var name = req.body.name;
    var image = req.body.image;
-   var newCampground = {name: name, image: image};
+   var desc = req.body.description;
+   var newCampground = {name: name, image: image, description: desc};
    Campground.create(newCampground, function(err, newlyCreated){
       if(err){
          console.log(err);
@@ -45,6 +47,16 @@ app.post('/campgrounds', function(req, res){
 
 app.get('/campgrounds/new', function(req, res){
    res.render('new.ejs');
+});
+
+app.get('/campgrounds/:id', function(req, res){
+   Campground.findByID(req.params.id, function(err, foundCampground){
+      if(err){
+         console.log(err);
+      } else {
+         res.render('show', {campground: foundCampground});
+      }
+   });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
