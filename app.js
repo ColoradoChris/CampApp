48cba73345b16b2c4133078754bbf91b40a9fe45
Chris Kansas <ckansas@smcm.edu>
2018-03-31 18:07:00 +0000

    Fixed the show route.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var request = require('request');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
@@ -50,7 +49,7 @@ app.get('/campgrounds/new', function(req, res){
 });
 
 app.get('/campgrounds/:id', function(req, res){
-   Campground.findByID(req.params.id, function(err, foundCampground){
+   Campground.findById(req.params.id, function(err, foundCampground){
       if(err){
          console.log(err);
       } else {
