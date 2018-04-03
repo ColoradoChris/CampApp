d089767b6b952fea2ecd000be986926fc568d43f
Chris Kansas <ckansas@smcm.edu>
2018-04-03 17:37:28 +0000

    Moved the Campground Model into a module.

--- app.js
+++ app.js
@@ -2,16 +2,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost/camp_app');
-
-var campgroundSchema = new mongoose.Schema({
-   name: String,
-   image: String,
-   description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
