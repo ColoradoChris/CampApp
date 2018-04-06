99ab40503f7a0b8f2cfbcc272d0785130e8d6ba8
Chris Kansas <ckansas@smcm.edu>
2018-04-06 19:21:56 +0000

    Added comment editing.

--- routes/comments.js
+++ routes/comments.js
@@ -36,6 +36,30 @@ router.post('/', function(req, res){
    });
 });
 
+//Comment Edit Route
+router.get('/:comment_id/edit', function(req, res){
+   Comment.findById(req.params.comment_id, function(err, comment){
+      if(err){
+         console.log(err);
+         res.redirect('back');
+      } else {
+         res.render('comments/edit', {campground_id: req.params.id, comment: comment}); 
+      }
+   });
+});
+
+//Comment Update Route
+router.put('/:comment_id', function(req, res){
+   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+      if(err){
+         console.log(err);
+         res.redirect('back');
+      } else {
+         res.redirect('/campgrounds/' + req.params.id);
+      }
+   });
+});
+
 function isLoggedIn(req, res, next){
    if(req.isAuthenticated()){
       return next();
